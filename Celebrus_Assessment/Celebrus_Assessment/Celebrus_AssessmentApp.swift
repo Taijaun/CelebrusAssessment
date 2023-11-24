140fa8e8317217b4aa0f3b93ddcb46189681cdbb
Taijaun Pitt <tjpitt4@gmail.com>
2023-11-24 16:29:03 +0000

    Created project, added MVVM structure

---
+++ Celebrus_Assessment/Celebrus_Assessment/Celebrus_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Celebrus_AssessmentApp.swift
+//  Celebrus_Assessment
+//
+//  Created by Tai Pitt on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Celebrus_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
